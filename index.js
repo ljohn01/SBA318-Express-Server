a0b596036d7935beadfd6d285e7cbda0a75e0ee1
ljohn01 <01.lauren.johnson@gmail.com>
2024-03-28 13:46:37 -0400

    Original commit message

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log('running app')
+// const http = require('http');
+
+// const app = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/html");
+//     res.write("Hello World");
+//     res.end();
+// }) 
+
+// app.listen(4444, () => {
+//     console.log('server is running on port 4444')
+// })
+
+function myCustomMiddleware(req, res, next) {
+   
+  }
+
